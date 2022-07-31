b4b73019994f69321bf1f4014a1d3b1ef4c63550
dacaceros97 <dalejandro1111@gmail.com>
2022-07-30 23:27:45 -0600

    mi primer commit

---
+++ src/leerarchivo/LeerArchivo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package leerarchivo;
+
+/**
+ *
+ * @author dcace
+ */
+public class LeerArchivo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        formLeer formulario = new formLeer();
+        formulario.setVisible(true);
+    }
+    
+}
